862e2f4ea0ae5a6256d9e8405e7e7b0b2168e8cf
Hugciksss <145644347+Hugciksss@users.noreply.github.com>
2024-05-09 10:07:42 +0300

    Update kalkulators.py

---
+++ kalkulators.py
@@ -0,0 +1,26 @@
+from tkinter import*
+
+mansLogs=Tk()
+mansLogs.title('Kalkulators')
+btn0=Button(mansLogs,text='0',padx='40',pady='20')
+btn0=Button(mansLogs,text='1',padx='40',pady='20')
+btn0=Button(mansLogs,text='2',padx='40',pady='20')
+btn0=Button(mansLogs,text='3',padx='40',pady='20')
+btn0=Button(mansLogs,text='4',padx='40',pady='20')
+btn0=Button(mansLogs,text='5',padx='40',pady='20')
+btn0=Button(mansLogs,text='6',padx='40',pady='20')
+btn0=Button(mansLogs,text='7',padx='40',pady='20')
+btn0=Button(mansLogs,text='8',padx='40',pady='20')
+btn0=Button(mansLogs,text='9',padx='40',pady='20')
+
+
+
+
+
+
+
+
+
+
+
+mansLogs.mainloop()
