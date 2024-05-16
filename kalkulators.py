f01417147a9fd798f818ca2dea90117fd32b544d
Hugciksss <145644347+Hugciksss@users.noreply.github.com>
2024-05-16 08:50:03 +0300

    pirma stunda

--- kalkulators.py
+++ kalkulators.py
@@ -22,16 +22,16 @@ def btnCommand(command):
 e=Entry(mansLogs,width=15,bd=10,font=("Arial Black",20),justify="right")
 e.grid(row=0,column=0,columnspan=4)
 
-btn0=Button(mansLogs,text='0',padx='40',pady='20')
-btn1=Button(mansLogs,text='1',padx='40',pady='20')
-btn2=Button(mansLogs,text='2',padx='40',pady='20')
-btn3=Button(mansLogs,text='3',padx='40',pady='20')
-btn4=Button(mansLogs,text='4',padx='40',pady='20')
-btn5=Button(mansLogs,text='5',padx='40',pady='20')
-btn6=Button(mansLogs,text='6',padx='40',pady='20')
-btn7=Button(mansLogs,text='7',padx='40',pady='20')
-btn8=Button(mansLogs,text='8',padx='40',pady='20')
-btn9=Button(mansLogs,text='9',padx='40',pady='20')
+btn0=Button(mansLogs,text='0',padx='40',pady='20',command=lambda:btnCommand(0))
+btn1=Button(mansLogs,text='1',padx='40',pady='20',command=lambda:btnCommand(1))
+btn2=Button(mansLogs,text='2',padx='40',pady='20',command=lambda:btnCommand(2))
+btn3=Button(mansLogs,text='3',padx='40',pady='20',command=lambda:btnCommand(3))
+btn4=Button(mansLogs,text='4',padx='40',pady='20',command=lambda:btnCommand(4))
+btn5=Button(mansLogs,text='5',padx='40',pady='20',command=lambda:btnCommand(5))
+btn6=Button(mansLogs,text='6',padx='40',pady='20',command=lambda:btnCommand(6))
+btn7=Button(mansLogs,text='7',padx='40',pady='20',command=lambda:btnCommand(7))
+btn8=Button(mansLogs,text='8',padx='40',pady='20',command=lambda:btnCommand(8))
+btn9=Button(mansLogs,text='9',padx='40',pady='20',command=lambda:btnCommand(9))
 
 btn7.grid(row=1,column=0)
 btn8.grid(row=1,column=1)
@@ -46,6 +46,27 @@ btn2.grid(row=3,column=1)
 btn3.grid(row=3,column=2)
 
 btn0.grid(row=4,column=0)
+
+btnSask=Button(mansLogs,text='+',padx='40',pady='20')
+btnAtn=Button(mansLogs,text='-',padx='40',pady='20')
+btnReiz=Button(mansLogs,text='*',padx='40',pady='20')
+btnDal=Button(mansLogs,text='/',padx='40',pady='20')
+btnC=Button(mansLogs,text='C',padx='40',pady='20')
+
+btnSask.grid(row=1,column=3)
+btnAtn.grid(row=2,column=3)
+btnReiz.grid(row=3,column=3)
+btnDal.grid(row=4,column=3)
+btnC.grid(row=4,column=1)
+
+def btnClick(number):
+   current=e.get()#nolasa skaitli
+   e.delete(0,END)#nodzēš
+   newNumber=str(current)+str(number)
+   e.insert(0,newNumber)#ievieto displejā
+   return 0
+ 
+ 
 
 
 
